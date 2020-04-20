3b198c09179b1b6e1e8be09d0c5e4cead64bf1c6
Vic <vickyorlo@gmail.com>
2020-04-20 23:44:29 +0200

    fix balance and spawning area issue

--- Scenes/Enemies/SpawnArea.gd
+++ Scenes/Enemies/SpawnArea.gd
@@ -6,9 +6,11 @@ extends Area2D
 # var b = "text"
 
 export(Array, PackedScene) var enemies
+var original_position = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	original_position = Vector2(position)
 	randomize()
 	pass # Replace with function body.
 
@@ -17,15 +19,18 @@ func _ready():
 #func _process(delta):
 #	pass
 
+func _physics_process(delta):
+	position = original_position + Vector2(0,Global.score/20)
+
 func _on_SpawnTimer_timeout():
 	var pos = Vector2()
-	pos.x = (randi() % int($CollisionShape2D.shape.extents.x))
-	pos.y = (randi() % int($CollisionShape2D.shape.extents.y))
+	pos.x = (randi() % int($CollisionShape2D.shape.extents.x*2))
+	pos.y = (randi() % int($CollisionShape2D.shape.extents.y*2))
 	var enemy = enemies[randi() % enemies.size()].instance()
-	enemy.global_position = pos + transform.get_origin() - $CollisionShape2D.shape.extents/2
+	enemy.global_position = pos + transform.get_origin() - $CollisionShape2D.shape.extents
 	enemy.set_as_toplevel(true)
 	add_child(enemy)
 
-	$SpawnTimer.wait_time = clamp(5.0 - Global.score/200,0.5,5)
+	$SpawnTimer.wait_time = clamp(5.0 - float(Global.score/400.0), 0.5, 5)
 
 
