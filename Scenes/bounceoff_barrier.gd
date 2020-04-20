8f98cfcb3dd0e0ee7379717d6ddf4d90ed6ce502
Vic <vickyorlo@gmail.com>
2020-04-20 21:36:01 +0200

    move the top barrier with score

--- Scenes/bounceoff_barrier.gd
+++ Scenes/bounceoff_barrier.gd
@@ -1,13 +1,16 @@
-extends StaticBody2D
+extends KinematicBody2D
 class_name bounceoff_barrier
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
+var original_position = Vector2()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	original_position = Vector2(position)
 	pass # Replace with function body.
 
 
@@ -15,6 +18,8 @@ func _ready():
 #func _process(delta):
 #	pass
 
+func _physics_process(delta):
+	position = original_position + Vector2(0,Global.score/20)
 
 func _on_bounceoff_body_entered(body):
 	if body.is_class("ball"):
